81f1e8056196cea13eca8d9bfb50d408ec1c39c3
myari211 <syukrihsb148@gmail.com>
2020-06-13 18:55:06 +0700

    add env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,23 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.startegy';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+    JwtModule.register({
+      secret: process.env.SECRETKEY,
+      signOptions: {
+        expiresIn: process.env.EXPIRESIN,
+      },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })

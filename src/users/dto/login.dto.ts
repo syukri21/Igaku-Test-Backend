e9250c08f080aebe1af64ecc3e08f8f8d44d44fc
myari211 <syukrihsb148@gmail.com>
2020-06-13 20:35:02 +0700

    fix register

--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -1,4 +1,13 @@
+import { IsEmail, IsDefined } from 'class-validator';
+import { Entity } from 'typeorm';
+
+@Entity()
 export class Login {
+  @IsDefined()
+  @IsEmail()
   email: string;
+
+
+  @IsDefined()
   password: string;
 }
--- src/users/dto/register.dto.ts
+++ src/users/dto/register.dto.ts
@@ -1,6 +1,19 @@
+import { Entity } from 'typeorm';
+import { IsDefined, IsEmail, Length } from 'class-validator';
+
+@Entity()
 export class Register {
+  @IsDefined()
   firstName: string;
+
+  @IsDefined()
   lastName: string;
+
+  @IsEmail()
+  @IsDefined()
   email: string;
+
+  @IsDefined()
+  @Length(6)
   password: string;
 }

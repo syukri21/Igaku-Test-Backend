d007612f66d72d11cfbbb3a977608bde86437dbd
myari211 <syukrihsb148@gmail.com>
2020-06-13 16:08:42 +0700

    add users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { Module } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { TodosModule } from './todos/todos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({}), TodosModule],
+  imports: [TypeOrmModule.forRoot({}), TodosModule, UsersModule],
+  providers: [UsersService],
+  controllers: [UsersController],
 })
 export class AppModule {
   constructor(private connection: Connection) {}

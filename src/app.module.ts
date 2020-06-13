d8018662135727750ea4a7ca0e13d213be1aa4a1
myari211 <syukrihsb148@gmail.com>
2020-06-13 17:01:38 +0700

    fix users todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { TodosModule } from './todos/todos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({}), TodosModule],
+  imports: [TypeOrmModule.forRoot({}), TodosModule, UsersModule],
 })
 export class AppModule {
   constructor(private connection: Connection) {}
